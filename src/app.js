1bdb8e82b9ed86abea11dd62f5d4f56dc93d925e
Imagineee <imagineeelego@gmail.com>
2022-06-11 20:52:36 +0100

    added a few new fixes

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ const { app, BrowserWindow, ipcMain, BrowserView, shell } = require('electron')
 var view
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 920,
+    width: 930,
     height: 800,
 		frame: false,
     fullscreenable: true,
     hasShadow: true,
     minimizable: true,
-    minWidth: 930,
-    minHeight: 800,
+    minWidth: 700,
+    minHeight: 600,
     defaultFontFamily: "monospace",
     webPreferences: {
       spellcheck: true,
@@ -29,8 +29,8 @@ const createWindow = () => {
   win.setBrowserView(view)
   view.setBounds({ x: 0, y: 30, width: 930, height: 800 - 30 })
   //if development
-  /* view.webContents.loadURL('http://localhost:3000/app/') */
-  view.webContents.loadURL('https://paper-clip.web.app/app/')
+  view.webContents.loadURL('http://localhost:3000/app/')
+  // view.webContents.loadURL('https://paper-clip.web.app/app/')
 	view.setAutoResize({ width: true, height: true })
   view.webContents.on('new-window', (event, url) => {
     event.preventDefault()
